dc6051e1bc8dd71ce6d4b76d94f6cfa9fb663f5b
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2016-02-06 11:53:54 +0100

    Started ArrayList example

---
+++ ninjalang-jvm/src/test/java/com/github/mbergenlid/ninjalang/jvm/examples/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.github.mbergenlid.ninjalang.jvm.examples;
+
+import com.github.mbergenlid.ninjalang.jvm.ClassGeneratorTestHelper;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+public class ArrayListTest {
+
+   @Test(expected = NoSuchElementException.class)
+   public void shouldCompile() throws IOException, ClassNotFoundException {
+      new ClassGeneratorTestHelper("examples/ArrayList").loadClass();
+   }
+}
