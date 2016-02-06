f7c16fe3bff72b9a5f356ca1192d18b72ed80518
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2016-02-06 15:40:22 +0100

    Upgraded to antlr version 4.5.2-1

--- ninjalang-parser/src/main/java/com/github/mbergenlid/ninjalang/parser/Parser.java
+++ ninjalang-parser/src/main/java/com/github/mbergenlid/ninjalang/parser/Parser.java
@@ -4,21 +4,13 @@ import com.github.mbergenlid.ninjalang.ClassBaseListener;
 import com.github.mbergenlid.ninjalang.ClassLexer;
 import com.github.mbergenlid.ninjalang.ClassParser;
 import com.github.mbergenlid.ninjalang.ast.ClassDefinition;
-import com.github.mbergenlid.ninjalang.ast.PrimaryConstructor;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.NotNull;
-import org.antlr.v4.runtime.misc.Nullable;
-import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.BitSet;
 
 public class Parser extends ClassBaseListener {
-
-   private final ClassDefinition.ClassDefinitionBuilder classDefinitionBuilder = ClassDefinition.builder();
 
    public static ClassDefinition classDefinition(final InputStream is) throws IOException {
       final ClassLexer l = new ClassLexer(new ANTLRInputStream(is));
