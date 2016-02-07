18cc8686c72eed616d27f1a0c12afe70edfd4c0a
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2016-02-07 10:36:55 +0100

    Forcing that types are only set once on TreeNodes.

--- ninjalang-parser/src/main/java/com/github/mbergenlid/ninjalang/ast/TreeNode.java
+++ ninjalang-parser/src/main/java/com/github/mbergenlid/ninjalang/ast/TreeNode.java
@@ -3,14 +3,24 @@ package com.github.mbergenlid.ninjalang.ast;
 import com.github.mbergenlid.ninjalang.ast.visitor.TreeVisitor;
 
 public abstract class TreeNode {
-   private Type type = Type.NO_TYPE;
+   private Type type = null;
 
    public void setType(Type type) {
+      if(this.type != null) {
+         throw new IllegalStateException("Not allowed to set type twice.");
+      }
       this.type = type;
    }
 
    public Type getType() {
+      if(type == null) {
+         throw new IllegalStateException("Tree hasn't been assigned a type yet.");
+      }
       return type;
+   }
+
+   public boolean hasType() {
+      return type != null;
    }
 
    public <T> T visit(final TreeVisitor<T> visitor) {
